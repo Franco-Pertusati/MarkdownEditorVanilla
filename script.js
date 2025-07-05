8ddb5a268a99e4ee73dd43e26739a42f787ce586
Franco Pertusati <pertusatifranco836@gmail.com>
2025-07-04 21:46:54 -0300

    Prueba implementacion de tecla arrowUp

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ const textCursor = document.getElementById("linePosition");
 var currentIndex = 0;
 
 const pageContent = [
-  { content: "# Title \n", id: 1 },
-  { content: "Una linea sin estilos \n", id: 2 },
-  { content: "Una palabra en **negrita** para probar \n", id: 3 },
-  { content: "Otra linea sin estilos \n", id: 2 },
+  { content: "# Title \n", id: 11 },
+  { content: "Una linea sin estilos \n", id: 12 },
+  { content: "Una palabra en **negrita** para probar \n", id: 13 },
+  { content: "Otra linea sin estilos \n", id: 14 },
 ];
 
 textCursor.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     lineBreak();
   }
-  if (event.key === "ArrowUp") {
-    moverCursor(1);
+  if (event.key === "ArrowUp" && currentIndex > 0) {
+    currentIndex--;
+    const referenceNode = document.getElementById(`line-${pageContent[currentIndex].id}`)
+    renderLineById(pageContent[currentIndex].id, referenceNode);
+    setCursorAtLine(pageContent[currentIndex].id);
   }
 });
-
-function moverCursor(value) {
-  if (value > 0) {
-  }
-}
 
 function renderAllLines() {
   pageContent.forEach((l) => {
     renderLine(l);
+    currentIndex++;
   });
   page.appendChild(textCursor);
+  console.log(currentIndex);
 }
 
 function focustextCursor() {
@@ -43,24 +43,23 @@ function lineBreak() {
     const newBr = document.createElement("br");
     page.appendChild(newBr);
   } else {
-    createLine(lineContent);
+    createLine(lineContent, true);
   }
 
   textCursor.value = "";
   page.appendChild(textCursor);
   textCursor.focus();
-  currentIndex++;
-  console.log(currentIndex);
 }
 
-function createLine(lineContent) {
+function createLine(lineContent, render) {
   const newLine = {
     content: lineContent,
     id: setId(),
   };
   pageContent.push(newLine);
-
-  renderLine(newLine);
+  if (render) {
+    renderLine(newLine);
+  }
 }
 
 function renderLine(line) {
@@ -72,13 +71,32 @@ function renderLine(line) {
   page.appendChild(newLine);
 }
 
+function setCursorAtLine(id) {
+  const line = document.getElementById(`line-${id}`);
+  line.remove();
+  textCursor.value = pageContent[currentIndex].content;
+}
+
+function renderLineById(id, referenceNode) {
+  console.log(referenceNode)
+  if (referenceNode === null) {
+    return
+  }
+  const line = pageContent.find((l) => l.id === id);
+  const newLine = document.createElement("div");
+
+  newLine.innerHTML = convertToHTML(line.content);
+  newLine.id = `line-${line.id}`;
+  page.insertBefore(newLine, referenceNode);
+}
+
 function convertToHTML(text) {
   text = text.replace(/^# (.*)/, "<h1>$1</h1>");
 
   text = text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
 
   if (!/^<h1>/.test(text)) {
-    text = `<p>${text}</p>`;
+    text = `<span>${text}</span>`;
   }
 
   return text;
